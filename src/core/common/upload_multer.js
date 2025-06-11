d5c7e23ba28397d7e9d603aa2432cbdc2effd3f9
priti sahu <priti.sahu@samyotech.com>
2025-06-11 18:55:45 +0530

    image is store in package section

--- src/core/common/upload_multer.js
+++ src/core/common/upload_multer.js
@@ -1,7 +1,7 @@
-import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
-const uploadDir = '././uploads';
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+const uploadDir = "././uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
@@ -10,44 +10,32 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + '-' + file.originalname);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
   },
 });
 
 export const upload = multer({
   storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 }
-}).single('image');
-
-
+  limits: { fileSize: 50 * 1024 * 1024 },
+}).single("image");
 
 export const logoUpload = multer({
   storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 }
-}).single('logoImage');
-
-
+  limits: { fileSize: 50 * 1024 * 1024 },
+}).single("logoImage");
 
 export const categoryUpload = multer({
   storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 }
-}).single('categoryImage');
-
+  limits: { fileSize: 50 * 1024 * 1024 },
+}).single("categoryImage");
 
 export const purchaseUpload = multer({
   storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 }
-}).single('PurchaseImage');
+  limits: { fileSize: 50 * 1024 * 1024 },
+}).single("PurchaseImage");
 
-
-
-
-
-
-
-
-
-
-
-  
+export const packageUpload = multer({
+  storage: storage,
+  limits: { fileSize: 50 * 1024 * 1024 },
+}).single("PackageImage");
--- src/services/package.js
+++ src/services/package.js
@@ -1,91 +1,91 @@
 import { PackageModelSchema } from "../models/package.js";
-import { errorCodes, Message, statusCodes, image_url } from "../core/common/constant.js";
+import {
+  errorCodes,
+  Message,
+  statusCodes,
+  image_url,
+} from "../core/common/constant.js";
 import CustomError from "../utils/exception.js";
 
-
-
-export const PackageData = async (req) =>{
-
-    const {name,description,price} = req.body;
-    if(!name || !description  || !price){
-         throw new CustomError(
-              statusCodes?.badRequest,
-              Message?.invalidInput,
-              errorCodes?.invalid_input
-            );
-}
-
-const ItemData = await PackageModelSchema.create({name,description,price, isDelete: false})
-return ItemData;
-}
-
-
-  
-export const getPackageData = async () =>{
-
-    const condition_obj = {isDelete:false}
-
-const ItemData = await PackageModelSchema.aggregate([
-    {$match :condition_obj},
-    {
-        $sort: {
-          createdAt: -1
-        }
-      }
-    
-]);
-return ItemData;
-}
-
-
-export const UpdatedPackage = async (req) =>{
-
-const {id} = req.params;
-const {name , description,price} = req.body;
-const AddItem = await PackageModelSchema.findById(id);
-
-if(!AddItem){
+export const PackageData = async (req) => {
+  const { name, description, price } = req.body;
+  if (!name || !description || !price) {
     throw new CustomError(
-        statusCodes?.notFound,
-        Message?.notFound,
-        errorCodes?.not_Found
-      );
-}
+      statusCodes?.badRequest,
+      Message?.invalidInput,
+      errorCodes?.invalid_input
+    );
+  }
 
-AddItem.name = name || AddItem.name;
-AddItem.description = description || AddItem.description;
-AddItem.price = price || AddItem.price;
+  const ItemData = await PackageModelSchema.create({
+    name,
+    description,
+    price,
+    isDelete: false,
+    PackageImage: req.file ? req.file.path : null,
+  });
+  return ItemData;
+};
 
+export const getPackageData = async () => {
+  const condition_obj = { isDelete: false };
 
+  const ItemData = await PackageModelSchema.aggregate([
+    { $match: condition_obj },
+    {
+      $addFields: {
+        imageUrl: {
+          $ifNull: [{ $concat: [image_url.url, "$PackageImage"] }, ""],
+        },
+      },
+    },
+  ]);
+  return ItemData;
+};
 
-const UpdatedData = await AddItem.save();
+export const UpdatedPackage = async (req) => {
+  const { id } = req.params;
+  const { name, description, price } = req.body;
+  const AddItem = await PackageModelSchema.findById(id);
 
-return UpdatedData;
-}
+  if (!AddItem) {
+    throw new CustomError(
+      statusCodes?.notFound,
+      Message?.notFound,
+      errorCodes?.not_Found
+    );
+  }
 
+  AddItem.name = name || AddItem.name;
+  AddItem.description = description || AddItem.description;
+  AddItem.price = price || AddItem.price;
+  if (req.file) AddItem.PackageImage = req.file.path;
 
- export const deletedPackage = async(req) =>{
+  const UpdatedData = await AddItem.save();
 
-    const {id} = req.params;
-    if(!id){
-          throw new CustomError(
-              statusCodes?.badRequest,
-             errorCodes?.invalid_input,
-             Message?.notFound
-            );
+  return UpdatedData;
+};
 
-    }
-    const findAddItem = await PackageModelSchema.findByIdAndUpdate(id,{ isDelete: true })
+export const deletedPackage = async (req) => {
+  const { id } = req.params;
+  if (!id) {
+    throw new CustomError(
+      statusCodes?.badRequest,
+      errorCodes?.invalid_input,
+      Message?.notFound
+    );
+  }
+  const findAddItem = await PackageModelSchema.findByIdAndUpdate(id, {
+    isDelete: true,
+  });
 
-    if(!findAddItem){
-        throw new CustomError(
-            statusCodes?.notFound, 
-            Message?.notFound,
-            errorCodes?.notFound
-          )
-    }
-    
-    return findAddItem;
+  if (!findAddItem) {
+    throw new CustomError(
+      statusCodes?.notFound,
+      Message?.notFound,
+      errorCodes?.notFound
+    );
+  }
 
-
- }
+  return findAddItem;
+};
